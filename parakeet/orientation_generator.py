fdee8bd5457454638b74800c72c0373c5d29a6d1
mjoosten1 <m.j.joosten@tudelft.nl>
2023-01-26 10:52:05 +0100

    restructuring the code step 1

---
+++ parakeet/orientation_generator.py
@@ -0,0 +1,20 @@
+
+
+### imports
+import numpy as np
+
+### class to generate orientations for a given molecule based on some specifications
+class orientation_generator(object):
+    def __init__(self):
+        pass
+
+    @classmethod       
+    def generate_inplane(self, n=1):
+        poses = []
+        for i in range(n):
+            phi = 0
+            theta = 0
+            psi = np.random.uniform(0, 2*np.pi)
+            poses.append((phi, theta, psi))
+        return poses
+        
