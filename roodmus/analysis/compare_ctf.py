2f8c40a973c82f88ae7d5622bf4d8a81ba9bd533
mjoosten1 <m.j.joosten@tudelft.nl>
2023-01-31 17:04:16 +0100

    adding support for .star files

--- roodmus/analysis/compare_ctf.py
+++ roodmus/analysis/compare_ctf.py
@@ -4,7 +4,7 @@
 def add_arguments(parser):
     parser.add_argument("--mrc-dir", help="directory with .mrc files and .yaml config files", type=str)
     parser.add_argument("--meta-file", help="particle metadata file. Can be .star (RELION) or .cs (CryoSPARC)", type=str)
-    parser.add_argument("--plot-file", help="output file name", type=str, default="ctf.png")
+    parser.add_argument("--plot-dir", help="output file name", type=str, default="ctf.png")
     return parser
 
 def get_name():
@@ -22,8 +22,8 @@ from roodmus.analysis.utils import IO
 ### functions
 def load_metadata(meta_file):
     if meta_file.endswith(".star"):
-        raise NotImplementedError
-        # metadata = IO.load_relion_star(meta_file)
+        metadata = IO.load_star(meta_file)
+        file_type = "star"
     elif meta_file.endswith(".cs"):
         metadata = IO.load_cs(meta_file)
         file_type = "cs"
@@ -42,10 +42,26 @@ def extract_from_metadata(metadata, file_type):
     return ugraph_paths, ctf
 
 def extract_from_config(config):
-    defocus = config["microscope"]["lens"]["c_10"] # in Angstroms
-    # the defous in Parakeet is a single value that is used for all particles in the micrograph
-    return defocus
+    defocusU = config["microscope"]["lens"]["c_10"] # in Angstroms
+    defocusV = defocusU
+    kV = config["microscope"]["beam"]["energy"] # in kV
+    Cs = config["microscope"]["lens"]["c_30"] # in mm
+    amp = 0 # not implemented in Parakeet as far as I know
+    Bfac = 0 # not implemented in Parakeet as far as I know
+    return np.array([defocusU, defocusV, kV, Cs, amp, Bfac])
     
+### plotting functions
+def plot_defocus_scatter(gt_ctf, ctf):
+    fig, ax = plt.subplots(1, 2, figsize=(10,5), sharey=True)
+    ax[0].scatter(gt_ctf[:,0], ctf[:,0], label="defocusU")
+    ax[0].plot([min(gt_ctf[:,0])*0.9, max(gt_ctf[:,0])*1.1], [min(gt_ctf[:,0])*0.9, max(gt_ctf[:,0])*1.1], color="black", linestyle="--", alpha=0.5)
+    ax[1].scatter(gt_ctf[:,1], ctf[:,1], label="defocusV")
+    ax[1].plot([min(gt_ctf[:,1])*0.9, max(gt_ctf[:,1])*1.1], [min(gt_ctf[:,1])*0.9, max(gt_ctf[:,1])*1.1], color="black", linestyle="--", alpha=0.5)
+    ax[0].grid()
+    ax[1].grid()
+    ax[0].set(xlabel="ground-truth defocus (A)", ylabel="estimated defocus (A)", title="defocusU")
+    ax[1].set(xlabel="ground-truth defocus (A)", title="defocusV")
+    return fig, ax
 
 ### main
 def main(args):
@@ -57,30 +73,26 @@ def main(args):
     metadata, file_type = load_metadata(args.meta_file)
     print("metadata loaded. extracting ctf values from metadata file")
     ugraph_paths, ctf = extract_from_metadata(metadata, file_type) # get the micrograph paths and the ctf values for each particle
-    gt_ctf = [] # list of ground-truth ctf values
     progressbar = tqdm(total=len(np.unique(ugraph_paths)), desc="loading ground-truth ctf values")
+    gt_ctf = []
     for ugraph_path in np.unique(ugraph_paths):
         num_particles_in_ugraph = np.sum(np.array(ugraph_paths) == ugraph_path)
         config = IO.load_config(os.path.join(args.mrc_dir, ugraph_path.replace(".mrc", ".yaml")))
-        gt_defocus = extract_from_config(config) # single value
-        gt_defocus = [gt_defocus] * num_particles_in_ugraph # convert to list
-        gt_ctf += gt_defocus # add to list
+        gt_ctf_ugraph = extract_from_config(config) # single value for the entire micrograph
+        _ = [gt_ctf.append(gt_ctf_ugraph) for _ in range(num_particles_in_ugraph)]
         _ = progressbar.update(1)
     progressbar.close()
+    gt_ctf = np.array(gt_ctf)   
+    print(f"ground-truth ctf values loaded. {gt_ctf.shape} ctf values found")
+    print(f"estimated ctf values loaded. {ctf.shape} ctf values found")
         
     ## plot the ground truth defocus values against the estimated defocus values
-    fig, ax = plt.subplots(1, 2, figsize=(10,5), sharey=True)
-    ax[0].scatter(gt_ctf, ctf[:,0], label="defocusU")
-    ax[0].plot([min(gt_ctf)*0.9, max(gt_ctf)*1.1], [min(gt_ctf)*0.9, max(gt_ctf)*1.1], color="black", linestyle="--", alpha=0.5)
-    ax[1].scatter(gt_ctf, ctf[:,1], label="defocusV")
-    ax[1].plot([min(gt_ctf)*0.9, max(gt_ctf)*1.1], [min(gt_ctf)*0.9, max(gt_ctf)*1.1], color="black", linestyle="--", alpha=0.5)
-    ax[0].grid()
-    ax[1].grid()
-    ax[0].set(xlabel="ground-truth defocus (A)", ylabel="estimated defocus (A)", title="defocusU")
-    ax[1].set(xlabel="ground-truth defocus (A)", title="defocusV")
-    fig.savefig(args.plot_file)
+    fig, ax = plot_defocus_scatter(gt_ctf, ctf)
+    fig.savefig(os.path.join(args.plot_dir, "defocus_scatter.png"))
     plt.close(fig)
     
 if __name__ == "__main__":
-    main(args)
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    main(add_arguments(parser).parse_args())
 
--- roodmus/analysis/utils.py
+++ roodmus/analysis/utils.py
@@ -4,14 +4,16 @@
 import os
 import yaml
 import numpy as np
-from typing import Optional
-from gemmi import cif
+# pipeliner
+from pipeliner.jobstar_reader import RelionStarFile
 
 ### i/o functions
 class IO(object):
     ## class containing several functions to load metadata from .star (RELION) and .cs (CryoSPARC) files 
     ## and the config file generated during the Parakeet simulation
     
+    
+    ## loading .cs files and parsing the ctf parameters, the particle positions and orientations
     @classmethod
     def load_cs(self, cs_path):
         metadata = np.load(cs_path)
@@ -28,7 +30,11 @@ class IO(object):
     def get_ctf_cs(self, metadata_cs):
         defocusU = metadata_cs["ctf/df1_A"]
         defocusV = metadata_cs["ctf/df2_A"]
-        return np.stack([defocusU, defocusV], axis=1)
+        kV = metadata_cs["ctf/accel_kv"]
+        Cs = metadata_cs["ctf/cs_mm"]
+        amp = metadata_cs["ctf/amp_contrast"]
+        Bfac = metadata_cs["ctf/bfactor"]
+        return np.stack([defocusU, defocusV, kV, Cs, amp, Bfac], axis=1)
 
     @classmethod
     def get_position_cs(self, metadata_cs):
@@ -43,132 +49,32 @@ class IO(object):
         pos = np.stack([x_abs, y_abs], axis=1)    
         return pos
 
+    # loading .star files and parsing the ctf parameters, the particle positions and orientations
+    def load_star(self, star_path):
+         return RelionStarFile(star_path)
+        
+    def get_ugraph_star(self, metadata_star):
+        particles = metadata_star.get_block("particles")
+        ugraph_paths = particles.column_as_list("_rlnMicrographName")
+        # convert to basename and remove index
+        ugraph_paths = [os.path.basename(path).decode("utf-8").split("_")[-1] for path in ugraph_paths]
+        return ugraph_paths
+    
+    def get_ctf_star(self, metadata_star):
+        optics = metadata_star.get_block("optics")
+        kV = optics.column_as_list("_rlnVoltage")
+        Cs = optics.column_as_list("_rlnSphericalAberration")
+        amp = optics.column_as_list("_rlnAmplitudeContrast")
+
+        particles = metadata_star.get_block("particles")
+        defocusU = particles.column_as_list("_rlnDefocusU")
+        defocusV = particles.column_as_list("_rlnDefocusV")
+        return np.stack([defocusU, defocusV, kV, Cs, amp], axis=1)
+
+    # loading the config file
     @classmethod
     def load_config(self, config_path):
         with open(config_path, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
         return config
-
-    @classmethod
-    def load_picked_particles_from_starfile(args, starfile: str, block_name: Optional[str]="particles", loop_name_prefix: Optional[str]="_rln")->dict:
-        if os.path.exists(starfile):
-            # load the starfile via gemmi 
-            gemmi_block = cif.read_file(starfile)[block_name]
-            """ 
-            Example loop headings
-            loop_ 
-            _rlnCoordinateX #1 
-            _rlnCoordinateY #2 
-            _rlnAutopickFigureOfMerit #3 
-            _rlnClassNumber #4 
-            _rlnAnglePsi #5 
-            _rlnImageName #6 
-            _rlnMicrographName #7 
-            _rlnOpticsGroup #8 
-            _rlnCtfMaxResolution #9 
-            _rlnCtfFigureOfMerit #10 
-            _rlnDefocusU #11 
-            _rlnDefocusV #12 
-            _rlnDefocusAngle #13 
-            _rlnCtfBfactor #14 
-            _rlnCtfScalefactor #15 
-            _rlnPhaseShift #16 
-            """
-            # create a dict to read these picked particles into
-            # make this indexed by micrograph index
-            picked_particles = {}
-
-            CoordinateX = gemmi_block.find_values(loop_name_prefix+"CoordinateX")
-            CoordinateY = gemmi_block.find_values(loop_name_prefix+"CoordinateY")
-            AutopickFigureOfMerit = gemmi_block.find_values(loop_name_prefix+"AutopickFigureOfMerit")
-            ClassNumber = gemmi_block.find_values(loop_name_prefix+"ClassNumber")
-            AnglePsi = gemmi_block.find_values(loop_name_prefix+"AnglePsi")
-            ImageName = gemmi_block.find_values(loop_name_prefix+"ImageName")
-            MicrographName = gemmi_block.find_values(loop_name_prefix+"MicrographName")
-            OpticsGroup = gemmi_block.find_values(loop_name_prefix+"OpticsGroup")
-            CtfMaxResolution = gemmi_block.find_values(loop_name_prefix+"CtfMaxResolution")
-            CtfFigureOfMerit = gemmi_block.find_values(loop_name_prefix+"CtfFigureOfMerit")
-            DefocusU = gemmi_block.find_values(loop_name_prefix+"DefocusU")
-            DefocusV = gemmi_block.find_values(loop_name_prefix+"DefocusV")
-            DefocusAngle = gemmi_block.find_values(loop_name_prefix+"DefocusAngle")
-            CtfBfactor = gemmi_block.find_values(loop_name_prefix+"CtfBfactor")
-            CtfScalefactor = gemmi_block.find_values(loop_name_prefix+"CtfScalefactor")
-            PhaseShift = gemmi_block.find_values(loop_name_prefix+"PhaseShift")
-            
-            # find the unique micrographs and create a dict with the
-            # micrograph indices as keys
-            # convert all entries to string instead of raw_string also
-            ugraph_names = []
-            for i in range(len(MicrographName)):
-                ugraph_names.append(MicrographName.str(i))
-            ugraph_names = np.array(ugraph_names, dtype=str)
-            # get the unique names and sort them
-            unique_ugraphs = sorted(np.unique(ugraph_names).tolist())
-            # take out only the basename and grab the index only (as string!)
-            for i in range(len(unique_ugraphs)):
-                unique_ugraphs[i]=os.path.basename(unique_ugraphs[i])[len(args.basename_prefix):-len(args.basename_suffix)]
-                # create a dict to hold particle property lists
-                picked_particles[unique_ugraphs[i]] = {}
-                picked_particles[unique_ugraphs[i]]["CoordinateX"] = []
-                picked_particles[unique_ugraphs[i]]["CoordinateY"] = []
-                picked_particles[unique_ugraphs[i]]["AutopickFigureOfMerit"] = []
-                picked_particles[unique_ugraphs[i]]["ClassNumber"] = []
-                picked_particles[unique_ugraphs[i]]["AnglePsi"] = []
-                picked_particles[unique_ugraphs[i]]["ImageName"] = []
-                picked_particles[unique_ugraphs[i]]["MicrographName"] = []
-                picked_particles[unique_ugraphs[i]]["OpticsGroup"] = []
-                picked_particles[unique_ugraphs[i]]["CtfMaxResolution"] = []
-                picked_particles[unique_ugraphs[i]]["CtfFigureOfMerit"] = []
-                picked_particles[unique_ugraphs[i]]["DefocusU"] = []
-                picked_particles[unique_ugraphs[i]]["DefocusV"] = []
-                picked_particles[unique_ugraphs[i]]["DefocusAngle"] = []
-                picked_particles[unique_ugraphs[i]]["CtfBfactor"] = []
-                picked_particles[unique_ugraphs[i]]["CtfScalefactor"] = []
-                picked_particles[unique_ugraphs[i]]["PhaseShift"] = []
-
-            # fill the dict lists with particle dicts
-            for i in range(len(CoordinateX)):
-                # grab the micrograph name and decipher which dict to add this particle to
-                ugraph_index = os.path.basename(MicrographName.str(i))[len(args.basename_prefix):-len(args.basename_suffix)]
-
-                picked_particles[ugraph_index]["CoordinateX"].append(CoordinateX.str(i))
-                picked_particles[ugraph_index]["CoordinateY"].append(CoordinateY.str(i))
-                picked_particles[ugraph_index]["AutopickFigureOfMerit"].append(AutopickFigureOfMerit.str(i))
-                picked_particles[ugraph_index]["ClassNumber"].append(ClassNumber.str(i))
-                picked_particles[ugraph_index]["AnglePsi"].append(AnglePsi.str(i))
-                picked_particles[ugraph_index]["ImageName"].append(ImageName.str(i))
-                picked_particles[ugraph_index]["MicrographName"].append(MicrographName.str(i))
-                picked_particles[ugraph_index]["OpticsGroup"].append(OpticsGroup.str(i))
-                picked_particles[ugraph_index]["CtfMaxResolution"].append(CtfMaxResolution.str(i))
-                picked_particles[ugraph_index]["CtfFigureOfMerit"].append(CtfFigureOfMerit.str(i))
-                picked_particles[ugraph_index]["DefocusU"].append(DefocusU.str(i))
-                picked_particles[ugraph_index]["DefocusV"].append(DefocusV.str(i))
-                picked_particles[ugraph_index]["DefocusAngle"].append(DefocusAngle.str(i))
-                picked_particles[ugraph_index]["CtfBfactor"].append(CtfBfactor.str(i))
-                picked_particles[ugraph_index]["CtfScalefactor"].append(CtfScalefactor.str(i))
-                picked_particles[ugraph_index]["PhaseShift"].append(PhaseShift.str(i))
-
-            for ugraph_index in picked_particles.keys():
-                picked_particles[ugraph_index]["CoordinateX"] = np.array(picked_particles[ugraph_index]["CoordinateX"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["CoordinateY"] =  np.array(picked_particles[ugraph_index]["CoordinateY"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["AutopickFigureOfMerit"] = np.array(picked_particles[ugraph_index]["AutopickFigureOfMerit"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["ClassNumber"] = np.array(picked_particles[ugraph_index]["ClassNumber"], dtype=str).astype(int).tolist()
-                picked_particles[ugraph_index]["AnglePsi"] = np.array(picked_particles[ugraph_index]["AnglePsi"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["ImageName"] = np.array(picked_particles[ugraph_index]["ImageName"], dtype=str).astype(str).tolist()
-                picked_particles[ugraph_index]["MicrographName"] = np.array(picked_particles[ugraph_index]["MicrographName"], dtype=str).astype(str).tolist()
-                picked_particles[ugraph_index]["OpticsGroup"] = np.array(picked_particles[ugraph_index]["OpticsGroup"], dtype=str).astype(int).tolist()
-                picked_particles[ugraph_index]["CtfMaxResolution"] = np.array(picked_particles[ugraph_index]["CtfMaxResolution"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["CtfFigureOfMerit"] = np.array(picked_particles[ugraph_index]["CtfFigureOfMerit"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["DefocusU"] = np.array(picked_particles[ugraph_index]["DefocusU"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["DefocusV"] = np.array(picked_particles[ugraph_index]["DefocusV"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["DefocusAngle"] = np.array(picked_particles[ugraph_index]["DefocusAngle"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["CtfBfactor"] = np.array(picked_particles[ugraph_index]["CtfBfactor"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["CtfScalefactor"] = np.array(picked_particles[ugraph_index]["CtfScalefactor"], dtype=str).astype(float).tolist()
-                picked_particles[ugraph_index]["PhaseShift"] = np.array(picked_particles[ugraph_index]["PhaseShift"], dtype=str).astype(float).tolist()
-
-        else:
-            print('File:\n{}Does not exist! Exiting!'.format(starfile))
-            exit(1)
-
-        return picked_particles
 
