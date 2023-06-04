b31efd9f8b68b582b34fee91a3d761c2b87c3469
Joel Greer <jg13994@my.bristol.ac.uk>
2023-06-04 21:11:10 +0000

    Added integration tests for analysis utilities for relion inputs

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -431,6 +431,243 @@ class IntegrationTestAnalysis(unittest.TestCase):
         os.environ["PATH"] = self.oldpath
         return super().tearDown()
 
+    def test_plot_ctf_star(self):
+        config_dir = os.path.join(
+            self.test_dir, "analysis_test_inputs/relion_subset_ugraphs"
+        )
+        meta_file = os.path.join(
+            self.test_dir,
+            "analysis_test_inputs/relion_subset/Extract/job007/particles.star",
+        )
+        plot_dir = os.path.join(self.test_dir, "ctf_star")
+        plot_types = "scatter ctf"
+
+        system_cmd = (
+            "roodmus plot_ctf"
+            + " --config_dir {}".format(config_dir)
+            + " --meta_file {}".format(meta_file)
+            + " --plot_dir {}".format(plot_dir)
+            + " --plot_types {}".format(plot_types)
+        )
+        print("system cmd: {}".format(system_cmd))
+        os.system(system_cmd)
+
+        # find the outputs
+        output_files: list = os.listdir(plot_dir)
+        output_files = sorted(output_files)
+        output_files = [
+            os.path.join(plot_dir, output_file) for output_file in output_files
+        ]
+        self.assertIsNotNone(output_files)
+        assert isinstance(output_files, list)
+        print("Output files: {}".format(output_files))
+
+        # find the reference files
+        ref_files: list = [
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/ctf_star/ctf_scatter.png",
+            ),
+            os.path.join(
+                self.test_data, "analysis_test_outputs/ctf_star/ctf_0.png"
+            ),
+        ]
+        ref_files = sorted(ref_files)
+
+        for output, ref in zip(output_files, ref_files):
+            assert filecmp.cmp(ref, output)
+
+    def test_plot_picking_star(self):
+        config_dir = os.path.join(
+            self.test_dir, "analysis_test_inputs/relion_subset_ugraphs"
+        )
+        meta_file = os.path.join(
+            self.test_dir,
+            "analysis_test_inputs/relion_subset/Extract/job007/particles.star",
+        )
+        job_types = "topaz_picking"
+        plot_dir = os.path.join(self.test_dir, "picking_star")
+        plot_types = (
+            "label_truth label_picked label_truth_and_picked precision "
+            + "boundary overlap"
+        )
+
+        system_cmd = (
+            "roodmus plot_picking"
+            + " --config_dir {}".format(config_dir)
+            + " --meta_file {}".format(meta_file)
+            + " --job_types {}".format(job_types)
+            + " --plot_dir {}".format(plot_dir)
+            + " --plot_types {}".format(plot_types)
+        )
+        print("system cmd: {}".format(system_cmd))
+        os.system(system_cmd)
+
+        # find the outputs
+        output_files: list = os.listdir(plot_dir)
+        output_files = sorted(output_files)
+        output_files = [
+            os.path.join(plot_dir, output_file) for output_file in output_files
+        ]
+        self.assertIsNotNone(output_files)
+        assert isinstance(output_files, list)
+        print("Output files: {}".format(output_files))
+
+        # find the reference files
+        ref_files: list = [
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/000000_truth.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/000000_\
+                    particles_picked.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/000000_\
+                    particles_truth_and_picked.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/precision.png",
+            ),
+            os.path.join(
+                self.test_data, "analysis_test_outputs/picking_star/recall.png"
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/precision_and_recall.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/f1_score.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/particles_boundary_x.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/particles_boundary_y.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/particles_boundary_z.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/particles_overlap.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/picking_star/overlap.png",
+            ),
+        ]
+        ref_files = sorted(ref_files)
+
+        for output, ref in zip(output_files, ref_files):
+            assert filecmp.cmp(ref, output)
+
+    def test_plot_classes_star(self):
+        config_dir = os.path.join(
+            self.test_dir, "analysis_test_inputs/relion_subset_ugraphs"
+        )
+        meta_file = os.path.join(
+            self.test_dir,
+            "analysis_test_inputs/relion_subset/Select/job009/particles.star",
+        )
+        job_types = "2D_class_selection"
+        plot_dir = os.path.join(self.test_dir, "classes_star")
+        plot_types = "precision frame_distribution"
+
+        system_cmd = (
+            "roodmus plot_classes"
+            + " --config_dir {}".format(config_dir)
+            + " --meta_file {}".format(meta_file)
+            + " --job_types {}".format(job_types)
+            + " --plot_dir {}".format(plot_dir)
+            + " --plot_types {}".format(plot_types)
+        )
+        print("system cmd: {}".format(system_cmd))
+        os.system(system_cmd)
+
+        # find the outputs
+        output_files: list = os.listdir(plot_dir)
+        output_files = sorted(output_files)
+        output_files = [
+            os.path.join(plot_dir, output_file) for output_file in output_files
+        ]
+        self.assertIsNotNone(output_files)
+        assert isinstance(output_files, list)
+        print("Output files: {}".format(output_files))
+
+        # find the reference files
+        ref_files: list = [
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/classes_star/2D_class_selection_2Dclass_\
+                precision.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/classes_star/2D_class_selection_2Dclass\
+                    _frame_distribution.png",
+            ),
+        ]
+        ref_files = sorted(ref_files)
+
+        for output, ref in zip(output_files, ref_files):
+            assert filecmp.cmp(ref, output)
+
+    def test_plot_alignment_star(self):
+        config_dir = os.path.join(
+            self.test_dir, "analysis_test_inputs/relion_subset_ugraphs"
+        )
+        meta_file = os.path.join(
+            self.test_dir,
+            "analysis_test_inputs/relion_subset/Extract/job013/particles.star",
+        )
+        plot_dir = os.path.join(self.test_dir, "alignment_star")
+
+        system_cmd = (
+            "roodmus plot_alignment"
+            + " --config_dir {}".format(config_dir)
+            + " --meta_file {}".format(meta_file)
+            + " --plot_dir {}".format(plot_dir)
+        )
+        print("system cmd: {}".format(system_cmd))
+        os.system(system_cmd)
+
+        # find the outputs
+        output_files: list = os.listdir(plot_dir)
+        output_files = sorted(output_files)
+        output_files = [
+            os.path.join(plot_dir, output_file) for output_file in output_files
+        ]
+        self.assertIsNotNone(output_files)
+        assert isinstance(output_files, list)
+        print("Output files: {}".format(output_files))
+
+        # find the reference files
+        ref_files: list = [
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/alignment_star/particles_picked_\
+                    pose_distribution.png",
+            ),
+            os.path.join(
+                self.test_data,
+                "analysis_test_outputs/alignment_star/true_pose_\
+                    distribution.png",
+            ),
+        ]
+        ref_files = sorted(ref_files)
+
+        for output, ref in zip(output_files, ref_files):
+            assert filecmp.cmp(ref, output)
+
 
 if __name__ == "__main__":
     unittest.main()
